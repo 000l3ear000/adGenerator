4c1a10cb649c79ef29033ae24641d0d4ab681514
Osama <osamanadeem679@gmail.com>
2021-04-22 04:32:11 +0500

    managed to centre the text in the given area

--- testMoviepy.py
+++ testMoviepy.py
@@ -1,8 +1,17 @@
 from moviepy.editor import *
 
+HEIGHT = 615
+FONTSIZE = 50
+
+
+def calculate_height(fontSize):
+    return HEIGHT - (fontSize / 2)
+
+
 # video2 = VideoFileClip("newTemplate.webM", audio=False)
-# video1 = VideoFileClip("newtempVideo.mp4", audio=False).subclip(0, 30)
-# video2 = VideoFileClip("newTemplate.webM", audio=False).set_opacity(0.9).resize(( 320, 240 ))
+# video1 = VideoFileClip("tempVideo.mp4", audio=False).subclip(0, 30)
+# video2 = VideoFileClip("newTemplate.webM", audio=False)
+# video2 = VideoFileClip("Template 6.mov", audio=False).resize(( 720, 720 ))
 # video2.show(10, interactive=True)
 # w1, h1 = video1.size
 # w2, h2 = video2.size
@@ -17,21 +26,21 @@ from moviepy.editor import *
 #             .set_position((364, 873))
 #             .set_duration(15))
 
-text_list = ["Piggy", "Kermit", "Gonzo", "Fozzie"]
+text_list = ["Kermit", "Gonzo", "Fozzie"]
 clip_list = []
-txt_clip = TextClip(" ", fontsize=50, color='white').set_duration(
-    5).set_position((364, 873))
+txt_clip = TextClip("Piggy", fontsize=FONTSIZE,
+                    color='white').set_duration(4.5).fadeout(2)
 clip_list.append(txt_clip)
 
 for text in text_list:
     try:
-        txt_clip = TextClip(text, fontsize=50, color='white').set_duration(
-            5).set_position((364, 873))
+        txt_clip = TextClip(text, fontsize=FONTSIZE,
+                            color='white').set_duration(6).fadeout(2)
         clip_list.append(txt_clip)
     except UnicodeEncodeError:
 
-        txt_clip = TextClip("Issue with text", fontsize=50, color='white').set_duration(
-            2).set_position((364, 873))
+        txt_clip = TextClip("Issue with text", fontsize=FONTSIZE,
+                            color='white').set_duration(5)
         clip_list.append(txt_clip)
 
 # txt_col = txt_clip.on_color(size=( 100, 100 ),
@@ -39,17 +48,20 @@ for text in text_list:
 
 # txt_mov = txt_col.set_pos( lambda t: (max(w1/30,int(w1-0.5*w1*t)),
 #                                   max(5*h1/6,int(100*t))) )
+video = VideoFileClip("output.mp4")
 final_clip = concatenate_videoclips(clip_list)
-video = VideoFileClip("finalVideo.mp4")
-result = CompositeVideoClip([video, final_clip.set_position((443, 880))])
+# video.show(10, interactive=True)
+result = CompositeVideoClip([video, final_clip.set_start(
+    5).set_position(("center", calculate_height(FONTSIZE)))])
+# result = CompositeVideoClip([video2])
 # result.set_duration = 30
 # audioclip = AudioFileClip("test.mp3")
 # new_audioclip = CompositeAudioClip([audioclip])
-# # # adding audio to the video clip
+# # adding audio to the video clip
 # result.audio = new_audioclip
 # result.show(10, interactive=True)
 # result.show(10, interactive=True)
-result.write_videofile("output.mp4", fps=25)
+result.write_videofile("finalVideo.mp4", fps=25)
 
 # # loading audio file
 
