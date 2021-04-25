bed39a4692734c4451f8b5d5b13dedba4fb095bf
Osama <osamanadeem679@gmail.com>
2021-04-25 17:30:06 +0500

    worked with the template with borders and diff animation

--- testMoviepy.py
+++ testMoviepy.py
@@ -1,4 +1,5 @@
 from moviepy.editor import *
+from moviepy.video import *
 
 HEIGHT = 615
 FONTSIZE = 50
@@ -9,16 +10,18 @@ def calculate_height(fontSize):
 
 
 # video2 = VideoFileClip("newTemplate.webM", audio=False)
-# video1 = VideoFileClip("tempVideo.mp4", audio=False).subclip(0, 30)
+video1 = VideoFileClip("greenTemplate.webM", audio=False)
 # video2 = VideoFileClip("newTemplate.webM", audio=False)
-# video2 = VideoFileClip("Template 6.mov", audio=False).resize(( 720, 720 ))
-# video2.show(10, interactive=True)
+video2 = VideoFileClip("video1.mp4", audio=False).resize((720, 720))
+# video1.show(10, interactive=True)
 # w1, h1 = video1.size
 # w2, h2 = video2.size
 # print(w1, h1, w2, h2)
 
 # video2 = VideoFileClip("newVideo.webM").resize((w/3, h/3)).set_opacity(.70)
 # masked_clip = video2.fx(vfx.mask_color, color=[71, 226, 211], thr=100, s=5)
+masked_clip = video1.fx(vfx.mask_color, color=[82, 254, 238], thr=100, s=5)
+# masked_clip = video2.fx.loop( n = 5 )
 # # video.write_videofile("test.mp4",fps=25)
 
 # # # w2, h2 = video2.size
@@ -26,16 +29,25 @@ def calculate_height(fontSize):
 #             .set_position((364, 873))
 #             .set_duration(15))
 
-text_list = ["Kermit", "Gonzo", "Fozzie"]
+# 4.5
+# 6
+
+text_list = ["Gonzo", "Fozzie"]
 clip_list = []
 txt_clip = TextClip("Piggy", fontsize=FONTSIZE,
-                    color='white').set_duration(4.5).fadeout(2)
+                    color='white').set_duration(3.8)
+clip_list.append(txt_clip)
+txt_clip = TextClip("Hello World!", fontsize=FONTSIZE,
+                    color='white').set_duration(4.6)
+clip_list.append(txt_clip)
+txt_clip = TextClip("Kermit!", fontsize=FONTSIZE,
+                    color='white').set_duration(6)
 clip_list.append(txt_clip)
 
 for text in text_list:
     try:
         txt_clip = TextClip(text, fontsize=FONTSIZE,
-                            color='white').set_duration(6).fadeout(2)
+                            color='white').set_duration(6.5)
         clip_list.append(txt_clip)
     except UnicodeEncodeError:
 
@@ -48,20 +60,21 @@ for text in text_list:
 
 # txt_mov = txt_col.set_pos( lambda t: (max(w1/30,int(w1-0.5*w1*t)),
 #                                   max(5*h1/6,int(100*t))) )
-video = VideoFileClip("output.mp4")
+video = VideoFileClip("finalVideo3.mp4")
 final_clip = concatenate_videoclips(clip_list)
 # video.show(10, interactive=True)
-result = CompositeVideoClip([video, final_clip.set_start(
-    5).set_position(("center", calculate_height(FONTSIZE)))])
-# result = CompositeVideoClip([video2])
-# result.set_duration = 30
-# audioclip = AudioFileClip("test.mp3")
-# new_audioclip = CompositeAudioClip([audioclip])
+# result = CompositeVideoClip([video, final_clip.set_start(1).set_position(("center", calculate_height(FONTSIZE)))])
+result = CompositeVideoClip([video2, masked_clip.set_opacity(.7), final_clip.set_start(
+    1).set_position(("center", calculate_height(FONTSIZE)))])
+# result = CompositeVideoClip([video1])
+# result.set_duration = 10
+audioclip = AudioFileClip("test.mp3")
+new_audioclip = CompositeAudioClip([audioclip])
 # # adding audio to the video clip
-# result.audio = new_audioclip
+result.audio = new_audioclip
 # result.show(10, interactive=True)
 # result.show(10, interactive=True)
-result.write_videofile("finalVideo.mp4", fps=25)
+result.write_videofile("finalVideo6.mp4", fps=30)
 
 # # loading audio file
 
